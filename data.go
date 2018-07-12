dcc3c2b774221ac142cc25b310ab0de61c14b131
Crystelite <38169753+DanellSerik@users.noreply.github.com>
2018-07-12 16:36:23 -0400

    Add files via upload

---
+++ data.go
@@ -0,0 +1,19 @@
+package main
+
+
+import (
+	"fmt"
+	
+	)
+	
+	
+	func avacado (s string) string {
+		return "Hello " + s
+		
+		}
+		
+		
+		func main () {
+			fmt.Println(avacado("Danell"))
+			
+			}
